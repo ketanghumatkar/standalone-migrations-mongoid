b10f0deacd1f028e47842e9907d74fddfe742986
Ketan Ghumatkar <ketan@treeni.com>
2017-09-16 11:25:30 +0530

    First draft

---
+++ lib/standalone/migrations/mongoid.rb
@@ -0,0 +1,9 @@
+require "standalone/migrations/mongoid/version"
+
+module Standalone
+  module Migrations
+    module Mongoid
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/standalone/migrations/mongoid/version.rb
@@ -0,0 +1,7 @@
+module Standalone
+  module Migrations
+    module Mongoid
+      VERSION = "0.1.0"
+    end
+  end
+end
